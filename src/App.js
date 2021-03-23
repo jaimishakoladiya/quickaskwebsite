cce9e2a247f9535d6de5e2200c8b7e97935fc413
chandblog <chandanikhanehsa@gamil.com>
2021-03-23 18:43:35 +0530

    try to run login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import Home from './component/home/Home'
 //import AppD from './component/interviews/AppD'
 import Login from './component/account/login/Login'
+import Registration from './component/account/register/Registartion'
 // import CompanyProfilePage from './component/companyprofile/CompanyProfilePage'
 
 // import Interviews from './component/interviews/Interviews'
@@ -10,8 +11,8 @@ function App() {
     <>
     {/* <CompanyProfilePage/> */}
    {/* <Home/> */}
-   {/* <AppD/> */}
-   <Login/>
+  <Registration/>
+   {/* <Login/> */}
 </>
     // <CompanyProfilePage/>
     // <Interviews/>
