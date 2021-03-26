9bc369de5ad2944782f3eec61b3012adbfb79952
jhkblog <jemishakoladiya2001@gmail.com>
2021-03-26 14:10:41 +0530

    conflict solve

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@
 // import Registration from './component/account/register/Registartion'
 // import CompanyProfilePage from './component/companyprofile/CompanyProfilePage'
 //import Navbar2 from './component/navbar/outernavbar/Navbar2'
-<<<<<<< HEAD
-// import { BrowserRouter } from 'react-router-dom';
-// import HomeNavbar from './component/navbar/homenavbar/HomeNavbar'
-=======
  import { BrowserRouter } from 'react-router-dom';
  import HomeNavbar from './component/navbar/homenavbar/HomeNavbar'
->>>>>>> d5b8784eed2a61f87f3856ce111745743050ae02
 //import Login from './component/account/login/Login';
 //import Forpass from './component/account/login/Forpass';
 // import { BrowserRouter } from 'react-router-dom';
@@ -23,14 +18,10 @@
 // import Navbar from './component/navbar/homenavbar/Navbar'
 //import CompanyProfilePage from './component/companyprofile/CompanyProfilePage'
 //import Navbar2 from './component/navbar/outernavbar/Navbar2'
-<<<<<<< HEAD
-// import Interviews from './component/interviews/Interviews'
-=======
 //import Interviews from './component/interviews/Interviews'
 
 //import Contact from './component/team/contact/Contact';
 
->>>>>>> d5b8784eed2a61f87f3856ce111745743050ae02
 function App() {
   return (
     <>
