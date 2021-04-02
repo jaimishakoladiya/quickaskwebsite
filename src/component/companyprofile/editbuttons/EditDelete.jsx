79b6f1bea42fbb135b743f592d0b745c0b660f49
Dhruvikakadiya <kakadiyadhruvi1700@gmail.com>
2021-04-02 18:17:26 +0530

    add Editdelte button

---
+++ src/component/companyprofile/editbuttons/EditDelete.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+
+function ViewDelete() {
+    return (
+        <>
+      <button style={{backgroundColor:"darkcyan",color:"white",width:"40px",height:"35px",borderRadius:"6px", borderColor:" darkcyan" }}>
+       <EditIcon/>
+         </button>
+         
+       <button style={{backgroundColor:" #f8384b",color:"white",width:"40px",height:"35px",marginLeft:"4px",borderRadius:"6px",borderColor:"#dc3545"}}>
+         <DeleteIcon/>
+         </button>
+         
+        
+        </>
+    )
+}
+
+export default ViewDelete
