97bcab8a5a991652d6d4d24347112a6048a35c50
Dhruvi kakadiya <kakadiyadhruvi1700@gmail.com>
2021-04-20 11:00:13 +0530

    ..

--- src/redux/reducers/companyprofile/companyprofileReducer.js
+++ src/redux/reducers/companyprofile/companyprofileReducer.js
@@ -1,8 +1,8 @@
 import {
     ADD_DEPT_QUESTIONS, DELETE_DEPT_QUESTIONS, ADD_JOB_QUESTIONS, DELETE_JOB_QUESTIONS
-   ,ADD_MANAGER_DATA
-    , ADD_MANAGER_QUESTIONS, DELETE_MANAGER_QUESTIONS, ADD_DEPT_DATA,ADD_JOB_DATA, EDIT_DEPT_DATA,
-    EDIT_MANAGER_DATA,DELETE_JOB_DATA, EDIT_JOB_DATA, DELETE_QUESTION, DELETE_DEPT_DATA,DELETE_MANAGER_DATA, GET_DEPT_DATA
+    , ADD_MANAGER_DATA
+    , ADD_MANAGER_QUESTIONS, DELETE_MANAGER_QUESTIONS, ADD_DEPT_DATA, ADD_JOB_DATA, EDIT_DEPT_DATA,
+    EDIT_MANAGER_DATA, DELETE_JOB_DATA, EDIT_JOB_DATA, DELETE_QUESTION, DELETE_DEPT_DATA, DELETE_MANAGER_DATA, GET_DEPT_DATA
 } from "../../types/companyprofile/companyprofileTypes";
 import update from "react-addons-update"
 
@@ -10,12 +10,12 @@ const intitialstate = {
     deptquestion: [],
     jobquestion: [],
     managerquestion: [],
-    deptdata:[],
-    managerdata:[],
-    jobdata:[],
-    deletedept:[],
-    deletejobdata:[],
-    users:[],
+    deptdata: [],
+    managerdata: [],
+    jobdata: [],
+    deletedept: [],
+    deletejobdata: [],
+    users: [],
 }
 
 const companyprofileReducer = (state = intitialstate, action) => {
@@ -58,104 +58,104 @@ const companyprofileReducer = (state = intitialstate, action) => {
                 ...state,
                 managerquestion: state.managerquestion.filter((item, id) => id !== action.payload)
             }
-        
+
         case ADD_DEPT_DATA:
-            return{
+            return {
                 ...state,
-                deptdata:[...state.deptdata,action.payload]
+                deptdata: [...state.deptdata, action.payload]
             }
-         case ADD_MANAGER_DATA:
-                return{
-                    ...state,
-                    managerdata:[...state.managerdata,action.payload]
-                }
-            case ADD_JOB_DATA:
-                return{
-                    ...state,
-                    jobdata:[...state.jobdata,action.payload]
-                }
-            case EDIT_DEPT_DATA:
-                return update(state,{
-                       deptdata:{
-                           [action.id]:{
-                               $set :action.payload
-                           }
-                       }
-                   }
-                )
-            case EDIT_JOB_DATA:
-                return update(state,{
-                    jobdata:{
-                        [action.id]:{
-                            $set:action.payload
-                        }
+        case ADD_MANAGER_DATA:
+            return {
+                ...state,
+                managerdata: [...state.managerdata, action.payload]
+            }
+        case ADD_JOB_DATA:
+            return {
+                ...state,
+                jobdata: [...state.jobdata, action.payload]
+            }
+        case EDIT_DEPT_DATA:
+            return update(state, {
+                deptdata: {
+                    [action.id]: {
+                        $set: action.payload
                     }
                 }
+            }
             )
-            case EDIT_MANAGER_DATA:
-                return update(state,{
-                    managerdata:{
-                        [action.id]:{
-                            $set:action.payload
-                        }
+        case EDIT_JOB_DATA:
+            return update(state, {
+                jobdata: {
+                    [action.id]: {
+                        $set: action.payload
                     }
-                })
-            case DELETE_QUESTION:
-              
-                switch(action.payload){
-                    case "dept" :
-                        const n=state.deptdata[action.userid].newque.filter((item,index)=>index !== action.queid)
-                       
-                        return update(state,{
-                            deptdata:{
-                                [action.userid]:{
-                                        newque:{
-                                            $set:n
-                                        }
-                                }
-                            }
-                        })
-                     case "job":
-                         const n1=state.jobdata[action.userid].newque.filter((item,index)=>index !== action.queid) 
-
-                         return update(state,{
-                             jobdata:{
-                                 [action.userid]:{
-                                     newque:{
-                                         $set:n1
-                                     }
-                                 }
-                             }
-                         })
                 }
-                case DELETE_DEPT_DATA:
-                    return{
-                        ...state,
-                       deptdata: state.deptdata.filter((item, id) => id !== action.payload)
+            }
+            )
+        case EDIT_MANAGER_DATA:
+            return update(state, {
+                managerdata: {
+                    [action.id]: {
+                        $set: action.payload
                     }
-                    case DELETE_JOB_DATA:
-                        return{
-                            ...state,
-                            jobdata: state.jobdata.filter((item, id) => id !== action.payload)
-                        }
-                        case DELETE_MANAGER_DATA:
-                            return{
-                                ...state,
-                                managerdata: state.managerdata.filter((item,id) => id !== action.payload)
+                }
+            })
+        case DELETE_QUESTION:
 
-                            }
-                            case GET_DEPT_DATA:
-                                return {
-                                    users:action.payload
+            switch (action.payload) {
+                case "dept":
+                    const n = state.deptdata[action.userid].newque.filter((item, index) => index !== action.queid)
+
+                    return update(state, {
+                        deptdata: {
+                            [action.userid]: {
+                                newque: {
+                                    $set: n
                                 }
+                            }
+                        }
+                    })
+                case "job":
+                    const n1 = state.jobdata[action.userid].newque.filter((item, index) => index !== action.queid)
+
+                    return update(state, {
+                        jobdata: {
+                            [action.userid]: {
+                                newque: {
+                                    $set: n1
+                                }
+                            }
+                        }
+                    })
+            }
+        case DELETE_DEPT_DATA:
+            return {
+                ...state,
+                deptdata: state.deptdata.filter((item, id) => id !== action.payload)
+            }
+        case DELETE_JOB_DATA:
+            return {
+                ...state,
+                jobdata: state.jobdata.filter((item, id) => id !== action.payload)
+            }
+        case DELETE_MANAGER_DATA:
+            return {
+                ...state,
+                managerdata: state.managerdata.filter((item, id) => id !== action.payload)
+
+            }
+        case GET_DEPT_DATA:
+            return {
+                users: action.payload
+            }
         default:
             return {
                 ...state
             }
-          
 
 
-            
+
+
     }
 }
 
