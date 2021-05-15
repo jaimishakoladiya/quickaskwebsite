70f573e8ecdae24e00832fcb6ce39ec50fe7f82b
bansiblog <71442566+bansiblog@users.noreply.github.com>
2021-05-15 14:43:41 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ import HomeNavbar from './component/navbar/homenavbar/HomeNavbar'
   import { Provider } from "react-redux";
  import store from "./redux/Store";
  //import StartInterviewTips from "./component/videoupload/StartInterviewTips"
-// import StartInterview from './component/videoupload/Startinterview';
+ import Startinterview from './component/videoupload/Startinterview';
 //import PractiseInterviewTips from './component/videoupload/PractiseInterviewTips';
 //import FinishInterview from './component/videoupload/FinishInterview';
 //import InterviewQuestion from './component/videoupload/InterviewQuestion';
